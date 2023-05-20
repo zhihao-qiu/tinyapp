72b16a2604ba090cbd947b3206e0fb2fe597076c
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-19 21:51:25 -0700

    updated according to review

---
+++ database.js
@@ -0,0 +1,40 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "user3RandomID",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user3RandomID",
+  },
+  "8rc4wJ": {
+    longURL: "https://www.npmjs.com/",
+    userID: "user2RandomID",
+  },
+  "c3yWm4": {
+    longURL: "https://expressjs.com/",
+    userID: "user3RandomID",
+  },
+};
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "$2a$10$0TpJRWm/RtfOBuKGH25c/OxPllh5jXn/HGPNTUklZ4t99EsICk5tS",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2a$10$gqOlkhztQ.b30a6mwmWH3.y0MFbOng0dPmgW.B6.ISfvsYtro2a2W",
+  },
+  user3RandomID: {
+    id: "user3RandomID",
+    email: "a@a",
+    password: "$2a$10$EWCJ3UbIzNtcatdLA9IhF.URVwyzYoP269im9nk8Gc9pOk6ASn0L6",
+  },
+};
+
+module.exports = {
+  urlDatabase,
+  users,
+};
--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const morgan = require('morgan');
 const express = require("express");
-const app = express();
-const PORT = 8080;
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
+const { urlDatabase, users } = require("./database");
+const app = express();
+const PORT = 8080;
+
 const { generateRandomString,
   getUserByEmail,
   getUserNameByID,
@@ -22,42 +24,6 @@ app.use(cookieSession({
 
 const urlIDLength = 6;
 const userIDLength = 13;
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "user3RandomID",
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    userID: "user3RandomID",
-  },
-  "8rc4wJ": {
-    longURL: "https://www.npmjs.com/",
-    userID: "user2RandomID",
-  },
-  "c3yWm4": {
-    longURL: "https://expressjs.com/",
-    userID: "user3RandomID",
-  },
-};
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "$2a$10$0TpJRWm/RtfOBuKGH25c/OxPllh5jXn/HGPNTUklZ4t99EsICk5tS",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "$2a$10$gqOlkhztQ.b30a6mwmWH3.y0MFbOng0dPmgW.B6.ISfvsYtro2a2W",
-  },
-  user3RandomID: {
-    id: "user3RandomID",
-    email: "a@a",
-    password: "$2a$10$EWCJ3UbIzNtcatdLA9IhF.URVwyzYoP269im9nk8Gc9pOk6ASn0L6",
-  },
-};
-
 
 /**
  * Function of dealing with POST
@@ -74,25 +40,24 @@ app.post("/urls", (req, res) => {
 
   if (!urlDatabase[newID]) {
     urlDatabase[newID] = {};
-    urlDatabase[newID].longURL = "http://" + req.body.longURL;
+    urlDatabase[newID].longURL = req.body.longURL.startsWith("http://") ? req.body.longURL : ("http://") + req.body.longURL;
     urlDatabase[newID].userID = req.session.user_id;
   }
 
-  // res.send(urlDatabase); // Respond with 'Ok' (we will replace this)
-  res.redirect('/urls');
+  res.redirect(`/urls/${newID}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const url_id = req.params.id;
-  if (!req.session.user_id) return res.redirect('/login');
+  if (!req.session.user_id) return res.status(401).send("Please click here to <a href='/login'>login</a>!");
   if (urlDatabase[url_id] && urlDatabase[url_id].userID !== req.session.user_id) return res.status(400).send('This is not your URL');
 
   delete urlDatabase[url_id];
   return res.redirect('/urls');
 });
 
-app.post("/urls/:id/update", (req, res) => {
-  if (!req.session.user_id) return res.redirect('/login');
+app.post("/urls/:id/", (req, res) => {
+  if (!req.session.user_id) res.status(401).send("Please click here to <a href='/login'>login</a>!");
   if (urlDatabase[req.params.id] && urlDatabase[req.params.id].userID !== req.session.user_id) return res.status(400).send('This is not your URL');
 
   urlDatabase[req.params.id].longURL = req.body.longURL;
@@ -163,8 +128,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  // If the user is not logged in, redirect GET /urls/new to GET /login
-  if (!req.session.user_id) return res.redirect('/login');
+  if (!req.session.user_id) return res.status(401).send("Please click here to <a href='/login'>login</a>!");
   const templateVars = {
     username: req.session.user_id ? getUserNameByID(req.session.user_id, users) : "",
     urls: urlsForUser(req.session.user_id, urlDatabase, users)
